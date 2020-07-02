ff5c13066bc68569862b66d18123a980597b0922
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-02 17:44:01 +0200

    2

--- EX_02/utiliser-objets.php
+++ EX_02/utiliser-objets.php
@@ -4,8 +4,22 @@
     </head>
     
     <body>
+            <h1> Date 1 </h1>
         <?php
-                echo "Aujourd'hui, nous sommes le " . date("l d/m - H : i") ;
+                    
+                echo "<p> Aujourd'hui, nous sommes le " . date("Y/m/d H : i : s </p>") ;
+                echo "L'ISCC a débuté le 22 juin 2020 à 9h00.";
+
+            class date1 {
+                function date_1 ($date_ajd) {
+                    echo "<p> Aujourd'hui, nous sommes le " . date("Y/m/d H : i : s </p>") ;
+                 }
+             }
+            
+            $date_deb_ISCC = new date1;
+            $date_deb_ISCC -> date_1();
+
+
 
         ?>
     </body>
