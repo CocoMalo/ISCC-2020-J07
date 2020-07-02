5151034445c1556ee9388e25c30954f3c5f1537f
CocoMalo <67273799+CocoMalo@users.noreply.github.com>
2020-07-02 17:27:06 +0200

    Exo 02 - Utilisation objet

---
+++ EX_02/utiliser-objets.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html> 
+<html>
+    <head>
+    </head>
+    
+    <body>
+        <?php
+                echo "Aujourd'hui, nous sommes le " . date("l d/m - H : i") ;
+
+        ?>
+    </body>
+</html> 
